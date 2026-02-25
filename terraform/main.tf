7f34b6e7d1bf3db827ac48efed3873ac687771c5
Andrii Shevchuk <homehoneyhome@andriis-macbook-air-1.home>
2026-02-25 15:33:20 +0100

    adding tags to resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 resource "azurerm_resource_group" "pet_project_rg" {
   name     = "rg-devops-pet-project"
   location = "West Europe"
+
+  tags = {
+    environment = "dev"
+    owner = "Andrew" 
+    project = "first-pet-project"
+  }
 }
