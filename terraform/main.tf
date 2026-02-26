531bebb51f66860f01432e6cfad485c1643267fb
Andrii Shevchuk <homehoneyhome@andriis-macbook-air.home>
2026-02-26 15:06:26 +0100

    feat: creating db

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,3 +85,54 @@ resource "azurerm_container_group" "fastapi_cg" {
 output "app_url" {
   value = "http://${azurerm_container_group.fastapi_cg.ip_address}:8000"
 }
+
+# 1. Генератор пароля для базы (чтобы не хранить его в коде)
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# 2. Сервер PostgreSQL
+resource "azurerm_postgresql_flexible_server" "db_server" {
+  name                   = "psql-pet-project-${random_string.acr_suffix.result}"
+  resource_group_name    = azurerm_resource_group.pet_project_rg.name
+  location               = azurerm_resource_group.pet_project_rg.location
+  version                = "14"
+  
+  administrator_login    = "psqladmin"
+  administrator_password = random_password.db_password.result
+
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms" # Самый дешевый тариф (~$15/мес при 24/7)
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+}
+
+# 3. База данных внутри сервера
+resource "azurerm_postgresql_flexible_server_database" "pet_db" {
+  name      = "fastapi_db"
+  server_id = azurerm_postgresql_flexible_server.db_server.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
+}
+
+# 4. Разрешаем доступ из интернета (для тестов) 
+# В идеале нужно делать через Service Endpoint, но для начала упростим
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_access" {
+  name             = "allow-all-azure"
+  server_id        = azurerm_postgresql_flexible_server.db_server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
+
+# Выводим пароль (только для тебя, в консоль GitHub)
+output "db_password" {
+  value     = random_password.db_password.result
+  sensitive = true
+}
+
+output "db_host" {
+  value = azurerm_postgresql_flexible_server.db_server.fqdn
+}
