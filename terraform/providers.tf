b4de47909a229d98c021c2716c1c86fa1ab3e094
Andrii Shevchuk <homehoneyhome@andriis-macbook-air.home>
2026-02-26 20:18:09 +0100

    changed to statick name in providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   # --- ДОБАВЛЯЕМ ЭТОТ БЛОК ---
   backend "azurerm" {
-    resource_group_name  = "rg-devops-pet-project"
+    resource_group_name  = "rg-devops-pet-2d5ad629" 
     storage_account_name = "petproject1" # Вставь имя, которое создал выше
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
