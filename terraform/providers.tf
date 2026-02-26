7fba0e6cf4396815abb27f8daaa137fb17df012f
Andrii Shevchuk <homehoneyhome@andriis-macbook-air.home>
2026-02-26 20:22:12 +0100

    san

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
   # --- ДОБАВЛЯЕМ ЭТОТ БЛОК ---
   backend "azurerm" {
     resource_group_name  = "rg-devops-pet-2d5ad629" 
-    storage_account_name = "petproject1" # Вставь имя, которое создал выше
+    storage_account_name = "petproject2d5ad629" # Вставь имя, которое создал выше
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
